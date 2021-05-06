93330aedeb9cf2e29177cf65ec00806e9cb6dadb
JunYeob <junyeob@uw.edu>
2021-05-05 18:12:03 -0700

    api doc

---
+++ apidoc/api_project.js
@@ -0,0 +1,24 @@
+define({
+  "name": "TCSS 450 Lab Web Service",
+  "version": "0.1.0",
+  "description": "The TCSS 450 Lab Web Service API Documentation",
+  "title": "Lab API Doc",
+  "url": "https://junyeob-tcss450-labs.herokuapp.com/",
+  "order": [
+    "Hello",
+    "Params",
+    "Wait",
+    "DemoSql",
+    "Auth",
+    "Phish"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-05-06T01:10:59.678Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
